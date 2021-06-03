dd4518790725c013790f391fa34c74c66f7733d9
IhorKov <apasniy1234@gmail.com>
2021-06-03 13:50:11 +0300

    Initial commit

---
+++ src/Tasks/Work01/NumFubinachi.java
@@ -0,0 +1,27 @@
+package Tasks.Work01;
+
+public class NumFubinachi {
+
+    public static void main(String[] args) {
+            getFubinachi(50);
+    }
+
+    public static void getFubinachi(int size){
+
+        int a = 1;
+        int [] arr = { 0 ,a };
+
+        while (a < size) {
+            System.out.println(arr[1]);
+            a += arr[0];
+            arr = chagePosition(arr, a);
+        }
+
+    }
+    public static int[] chagePosition(int[] arr, int a){
+
+        arr[0] = arr[1];
+        arr[1] = a;
+        return arr;
+    }
+}
